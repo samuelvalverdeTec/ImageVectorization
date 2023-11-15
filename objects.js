6d6cc7a637739d9a2deb803550fb4d38886ed4c2
samuelvalverdee <savalverde@estudiantec.cr>
2023-11-14 19:35:51 -0600

    mayoria de metodos y base de simulacion

--- objects.js
+++ objects.js
@@ -5,22 +5,35 @@ class Poblacion {
         this.max = maxGeneraciones;
     }
 
-    // Other methods and properties
+    start(){
+        console.log('\nStarting...\n');
+        createInitialGen();
+    }
 }
 
 class Generacion {
 
-    constructor(num, max, target) {
+    constructor(num, maximo, target) {
         this.individuos = [];           // array de individuos
         this.number = num;              // numero de generacion
-        this.max = max;                 // maximo de individuos
+        this.max = maximo;              // maximo de individuos
+        this.best = undefined;          // mejor individuo, este tendra el display en la interfaz
 
         this.matingPool = [];           // array con los individuos seleccionados
         this.crossable = [];            // array con los individuos que se van a cruzar
+        this.crossed = [];
         this.mutables = [];             // array con los individuos que van a mutar
+        this.mutated = [];
 
-        for (let i = 0; i < max; i += 1) {
-            this.individuos.push(new Individuo(target, width, height));
+        for (let i = 0; i < max; i++) {
+            this.individuos.push(new Individuo(target, width, height, this.number));
+        }
+    }
+
+    calculateMassiveFitness(){      // calcula el fitness de todos los individuos de la generacion
+
+        for(let i = 0; i < this.individuos.length; i++) {
+            this.individuos[i].fitness();
         }
     }
 
@@ -31,9 +44,31 @@ class Generacion {
         const numberOfElementsToAdd = Math.ceil((percentage / 100) * this.individuos.length);
 
         this.matingPool = this.individuos.slice(0, numberOfElementsToAdd);
+        
+        return this.matingPool;
     }
 
     selectCrossable(percentage){
+
+        const numberOfElementsToSelect = Math.ceil((percentage / 100) * this.matingPool.length);
+
+        // Fisher-Yates algorithm
+        for (let i = this.matingPool.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.matingPool[i], this.matingPool[j]] = [this.matingPool[j], this.matingPool[i]];
+        }
+
+        this.crossable = this.matingPool.slice(0, numberOfElementsToSelect);
+
+        return this.crossable;
+    }
+
+    crossover(){
+        return this.crossed;
+    }
+
+    selectMutable(percentage){  // incompleto, ahorita es un copy paste de selectCrossable por lo que si se deja asi estaria
+                                // agregando parte de los individuos agregados a this.crossable en this.mutable
 
         const numberOfElementsToSelect = Math.ceil((percentage / 100) * this.matingPool.length);
 
@@ -42,12 +77,13 @@ class Generacion {
             const j = Math.floor(Math.random() * (i + 1));
             [array[i], array[j]] = [array[j], array[i]];
         }
+        this.mutable = this.matingPool.slice(0, numberOfElementsToSelect);
 
-        this.crossable = this.matingPool.slice(0, numberOfElementsToSelect);
+        return this.mutable;
     }
 
-    crossover(percentage){
-
+    mutate(){
+        // mutar en generacion o en individuo?
     }
 
 }
@@ -56,23 +92,27 @@ class Individuo {
 
 	// width = mat.matSize[1]   50               height = mat.matSize[0]  30
     // fila*width*4  + columna * 4
-    constructor(target, width, height) {
+    constructor(target, width, height, gen) {
         this.target = target;
         this.score = 0;
+        this.gen = gen
         this.value = function() {
             const pixelArray = new Uint8Array(width * height * 4);
-			for (var iPixel = 0; iPixel < width * height; iPixel++) {
+
+            for (let iPixel = 0; iPixel < width * height; iPixel++) {
 				var random = Math.random();
 				pixelArray[(iPixel*4)] = random < 0.5 ? 0:255;
 				pixelArray[(iPixel*4)+1] = random < 0.5 ? 0:255;
 				pixelArray[(iPixel*4)+2] = random < 0.5 ? 0:255;
 				pixelArray[(iPixel*4)+3] = 255;
 			}
+
+            return pixelArray;
         }
     }
 
     fitness() {
-        for (let i = 0; i < min; i++) {
+        for (let i = 0; i < this.value.length; i++) {
             if (this.value[i] != this.target[i]) {
                 this.score += Math.abs(this.value[i] - this.target[i]);
                 // diferencia += 1; opcion 2
@@ -81,10 +121,30 @@ class Individuo {
         return this.score;
     }
 
-    mutate(percentage){
+    mutate(startIndex, endIndex){
+        // se calcula el numero de pixeles a revolver
+        const numPixelsToScramble = Math.floor((endIndex - startIndex + 1) / 4);
 
+        // se generan indices random
+        var randomIndices = [];
+        for (let i = 0; i < numPixelsToScramble; i++) {
+            const randomPixelIndex = Math.floor(Math.random() * numPixelsToScramble) * 4;
+            randomIndices.push(randomPixelIndex);
+        }
+
+        // se revuelven los pixeles
+        for (const randomIndex of randomIndices) {
+            for (let i = 0; i < 4; i++) {
+                if(i < 3){
+                    const pixelIndex = startIndex + randomIndex + i;
+                    // de 0 a 255 y viceversa
+                    this.value[pixelIndex] = Math.abs(255 - this.value[pixelIndex]);
+                } else{
+                    continue
+                }
+            }
+        }
     }
-
 }
 
 export { Poblacion, Generacion, Individuo };
--- script.js
+++ script.js
@@ -6,34 +6,47 @@ canvas.width = 450;
 canvas.height = 300;
 const imgInput = document.getElementById('imageInput');
 const image = new Image();
-const button = document.getElementById('input-button');
-const maxGens = document.getElementById('input1');      // maximo de generaciones
-const individuos = document.getElementById('input2');   // individuos de la poblacion
-const percIndivsSelected = document.getElementById('input3');   // % individuos seleccionados por generacion
-const percIndivsMutb = document.getElementById('input4');   // % individuos mutables
-const percIndivsComb = document.getElementById('input5');   // % individuos combinables
-const numGen = 0;
+const maxGens = document.getElementById('input1');                  // maximo de generaciones
+const individuos = document.getElementById('input2');               // individuos de la poblacion
+const percIndivsSelected = document.getElementById('input3');       // % individuos seleccionados por generacion
+const percIndivsMutb = document.getElementById('input4');           // % individuos mutables
+const percIndivsComb = document.getElementById('input5');           // % individuos combinables
+const numGen = 1;
 const found = false;
 
 // mat.matSize = (height, length)
 
+function algorithm(gen){    // simulacion
+    // setTimeout(#)
+    gen.calculateMassiveFitness();
+    var matingPool = gen.selection(percIndivsSelected);
+    var cruzables = gen.selectCrossable(percIndivsComb);
+    var mutables = gen.selectMutable(percIndivsMutb);
+    for(let i = 0; i < mutables.length; i++){
+        mutables[i].mutate();
+    }
+    /*
+    .
+    .
+    .
+    */
+    numGen += 1;
+    algorithm(new Generacion(numGen, individuos, mat));
+}
+
 function createInitialGen(){
 
-    var gen = new Generacion(numGen+1, individuos, mat);
+    var gen = new Generacion(numGen, individuos, mat);
+    algorithm(gen);
 
 }
 
-function start(){
+function startAlgorithm(){
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    const mat = cv.imread(scannedImg);
-    const width = mat.matSize[1];
-    const height = mat.matSize[0];
-
     var poblacion = new Poblacion(parseInt(maxGens.value));
-    console.log('\nStarting...\n');
-    createInitialGen();
+    poblacion.start();
 }
 
 imgInput.addEventListener('change', function(e) {
@@ -52,6 +65,12 @@ image.addEventListener('load', function(){
     ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
     const scannedImg = ctx.getImageData(0, 0, canvas.width, canvas.height);
     ctx.putImageData(scannedImg, 0, 0);
+    // se inicializan variables globales dependientes de la imagen que se carga
+    const mat = cv.imread(scannedImg);
+    const width = mat.matSize[1];
+    const height = mat.matSize[0];
+    const mutationX = (width*height*4)/3;   // un tercio del array
+    const mutationY = 2*(mutationX);        // dos tercios del array
 })
 
 function verifyParameters(){
@@ -62,7 +81,7 @@ function verifyParameters(){
         if(percsParameters != 100){
             alert('ERROR: La suma de los porcentajes ingresados debe representar un 100%. Inténtelo de nuevo!');
         } else{
-            start();
+            startAlgorithm();
         }
     } else{
         alert('ERROR: El algoritmo no puede iniciar hasta que se llenen todos los requisitos solicitados. Inténtelo de nuevo!');
